ee9493f02ea7036dd022b1c6803659e3c2394c1e
yakbuok <chuolbuokyak@gmail.com>
2024-09-25 16:14:20 +0300

    chuol-attendance

---
+++ src/expression.java
@@ -0,0 +1,8 @@
+public class expression {
+        public static void main(String[] args) {
+            int result = 50 + 50; // Change this expression to test others
+            System.out.println("The result is: " + result);
+        }
+    }
+
+
